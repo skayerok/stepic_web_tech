9abecf2f7cae7d93ce7b2a69a7b1937fa6babb55
Pavel <pavel-botsman@yandex.ru>
2016-05-11 09:16:42 +0300

    some changes

--- hello.py
+++
@@ -1,5 +0,0 @@
-def app(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    resp = env['QUERY_STRING'].split('&')
-    resp = [item + '\r\n' for item in resp]
-    return resp
