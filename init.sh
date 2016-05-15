81a015461d66dc0fbaed6c640f180d2b08d75c49
Pavel <pavel-botsman@yandex.ru>
2016-05-15 21:03:38 +0300

    removed redundant line in init

--- init.sh
+++ init.sh
@@ -3,4 +3,3 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 hello:app &
