115d908625cf07ea62ffdf1cb49e80ef226c5b2e
Pavel <pavel-botsman@yandex.ru>
2016-05-15 17:35:22 +0300

    added string for starting gunicorn

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+gunicorn -b 0.0.0.0:8080 hello:app &
