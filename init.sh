e137f1088f14eace21b6b44f91e56c0edcf75106
Pavel <pavel-botsman@yandex.ru>
2016-05-15 20:59:16 +0300

    test gunicorn new config

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 gunicorn -b 0.0.0.0:8080 hello:app &
