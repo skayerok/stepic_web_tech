eff196bfbe5efda4c35a5d300787ff3b91efa621
Pavel <pavel-botsman@yandex.ru>
2016-05-16 22:18:17 +0300

    some changes in config

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo ln -s /home/box/web/ask/gunicorn.conf  /etc/gunicorn.d/test2
 sudo /etc/init.d/gunicorn restart
