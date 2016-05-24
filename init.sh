b5c9a6ab73bea20c80ae0a0098dfc619b17d19ab
skayerok <pavel-botsman@yandex.ru>
2016-05-24 12:48:02 +0300

    replaced start with restart in mysql

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
 sudo ln -s /home/box/web/ask/gunicorn.conf  /etc/gunicorn.d/test2
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+sudo /etc/init.d/mysql restart
